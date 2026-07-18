d65a102d204b76f57cb4c93522f9a0955e0948f6
Ritika-Nigwal <parisramnigwal@gmail.com>
2026-07-18 11:14:39 +0530

    again error solved

--- backend/app/core/imageUpload.py
+++ backend/app/core/imageUpload.py
@@ -6,9 +6,10 @@ load_dotenv()
 
 SUPABASE_URL=os.getenv("SUPA_BASE_URL")
 SUPABASE_KEY=os.getenv("SUPA_BASE_SECRET_KEY")
+print(f"{SUPABASE_URL}/storage/v1/object/public/images/")
 supabase=create_client(SUPABASE_URL,SUPABASE_KEY)
 async def upload_image(image:UploadFile=File(...)):
     contents=await image.read()
     supabase.storage.from_("images").upload(image.filename,contents)
     path=f"{SUPABASE_URL}/storage/v1/object/public/images/{image.filename}"
-    return {"file_name":path}
+    return {"filename":path}
